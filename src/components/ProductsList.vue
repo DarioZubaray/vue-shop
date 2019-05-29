4b0d80710289779e9c21a095a6648a9e25748fe6
Dario Zubaray <dzubaray@gmail.com>
2019-05-29 15:49:59 -0300

    Agregando los componentes de productos, removiendo el componente de ejemplo helloworld

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ products.length }}
+    </div>
+</template>
+
+<script>
+    import {mapActions, mapMutations, mapState} from 'vuex'
+    export default {
+        name: 'ProdutcsList',
+        mounted() {
+            this.fetchProducts()
+        }, 
+        data() {
+            return {
+                paginated: ['products'],
+                perPage: 3
+            }
+        },
+        computed: {
+            ...mapState('products', ['products'])
+        },
+        methods: {
+            ...mapActions('products', ['fetchProducts'])
+        }
+    }
+</script>
