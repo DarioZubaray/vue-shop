fd4a2f5df7c4c87eed62af3138e628e8d5b84f76
dario zubaray <dzubaray@gmail.com>
2019-06-02 11:29:42 -0300

    implementando pug en el shoplayout

--- src/components/ShopLayout.vue
+++ src/components/ShopLayout.vue
@@ -1,12 +1,9 @@
-<template>
-    <b-container>
-        <b-row>
-            <b-col cols="9">
-                <slot name="product-list"></slot>
-            </b-col>
-            <b-col cols="3">
-                <slot name="cart"></slot>
-            </b-col>
-        </b-row>
-    </b-container>
+<template lang="pug">
+    b-container
+        b-row
+            b-col(cols="9")
+                slot(name="product-list")
+            b-col(cols="3")
+                slot(name="cart")
+
 </template>
