a4e984049d99a80dbb4ca35e8b749a2c87526386
Dario Zubaray <dzubaray@gmail.com>
2019-06-03 08:38:35 -0300

    persistiendo vuex en indexedDB con localForage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import localForage from "localforage"
 import VuexPersistence from 'vuex-persist'
 import products from './modules/products'
 import cart from './modules/cart'
@@ -7,7 +8,7 @@ import cart from './modules/cart'
 Vue.use(Vuex)
 
 const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
+  storage: localForage,
   modules: ['cart']
 })
 
